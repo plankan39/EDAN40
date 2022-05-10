a62381178018c3bd53b3469778f8232d47a72a7d
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-05-10 13:08:15 +0200

    Folding fixed...

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -37,7 +37,7 @@ attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
 
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
-maximaBy valueFcn = foldr (maxiB valueFcn) []
+maximaBy valueFcn = foldl (maxiB valueFcn) []
     where
         maxiB _ [] x = [x]
         maxiB f acc x
