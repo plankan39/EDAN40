6a46cdb575b217563d278baf4c521a57cabdd651
Emil Jonathan Eriksson <eje1999@gmail.com>
2022-05-11 13:35:08 +0200

    Switch ' to be unoptimized functions, work on similarityScore

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -1,4 +1,5 @@
 import Data.List (intersperse)
+
 -- EDAN40 A2 String Alignment by Emil Eriksson (em5184er-s) && Lukas Elmlund (lu0804el-s)
 
 -- Hardcoded values
@@ -36,15 +37,41 @@ pairScore (_, []) = 0
 pairScore (x : xs, y : ys) = pairScore (xs, ys) + score x y
 
 -- Returns the score of the optimal alignment of two strings
+similarityScore' :: String -> String -> Int
+similarityScore' [] _ = 0
+similarityScore' _ [] = 0
+similarityScore' (s1 : s1s) (s2 : s2s) =
+  maximum
+    [ similarityScore' s1s s2s + score s1 s2,
+      similarityScore' s1s (s2 : s2s) + score s1 '-',
+      similarityScore' (s1 : s1s) s2s + score '-' s2
+    ]
+
 similarityScore :: String -> String -> Int
 similarityScore [] _ = 0
 similarityScore _ [] = 0
-similarityScore (s1 : s1s) (s2 : s2s) =
-  maximum
-    [ similarityScore s1s s2s + score s1 s2,
-      similarityScore s1s (s2 : s2s) + score s1 '-',
-      similarityScore (s1 : s1s) s2s + score '-' s2
-    ]
+similarityScore s1 s2 = simSco (length s1) (length s2) -- Lengths are indexes, we go from the back
+  where
+    simSco i j = simTable !! i !! j -- We get all data from the table
+    simTable = [[simEntry i j | j <- [0 ..]] | i <- [0 ..]] -- Generate table, where every entry is simEntry called on index
+
+    -- Entry of our simTable, takes indexes and returns score of that entry
+    simEntry :: Int -> Int -> Int
+    simEntry 0 _ = 0 -- We compare an empty list to some string in the table
+    simEntry _ 0 = 0
+    simEntry i j
+      -- These are where the scoring is done, we recursively look in the table
+      -- for the results. This is basically similarityScore', but we replace recursive
+      -- calls with calls in the table
+      | x == y = score x y + simSco (i - 1) (j - 1) -- Letters are the same; Don't try to insert space but look for rest of word (exact match is max score for one column)
+      | otherwise =
+        maximum
+          [ score x '-' + simSco i (j - 1),
+            score '-' y + simSco (i - 1) j
+          ]
+      where
+        x = s1 !! (i - 1) -- x and y are the Char at (i - 1) and (j - 1) in strings s1 and s2 respectively
+        y = s2 !! (j - 1)
 
 -- Attaches h1 and h2 to the list in fst and snd respectively
 -- in a list of pairs
@@ -65,27 +92,28 @@ maximaBy valueFcn = foldl (maxiB valueFcn) []
         currentMax = f (head acc)
 
 -- Returns a list of all optimal alignments between two strings
-optAlignments :: String -> String -> [AlignmentType]
-optAlignments [] _ = [("","")]
-optAlignments _ [] = [("","")]
-optAlignments (s1 : s1s) (s2 : s2s) =
+optAlignments' :: String -> String -> [AlignmentType]
+optAlignments' [] _ = [("", "")]
+optAlignments' _ [] = [("", "")]
+optAlignments' (s1 : s1s) (s2 : s2s) =
   maximaBy pairScore $
-    attachHeads s1 s2 (optAlignments s1s s2s) -- We don't insert any space here and find alignment for rest of the Strings
-    ++ attachHeads s1 '-' (optAlignments s1s (s2 : s2s)) -- We insert space instead first char of second string s2 and try to find the maximum for rest of s1 and the whole of s2
-    ++ attachHeads '-' s2 (optAlignments (s1 : s1s) s2s) -- Line above but we insert space instead of s1 instead
+    attachHeads s1 s2 (optAlignments' s1s s2s) -- We don't insert any space here and find alignment for rest of the Strings
+      ++ attachHeads s1 '-' (optAlignments' s1s (s2 : s2s)) -- We insert space instead first char of second string s2 and try to find the maximum for rest of s1 and the whole of s2
+      ++ attachHeads '-' s2 (optAlignments' (s1 : s1s) s2s) -- Line above but we insert space instead of s1 instead
 
 -- Optimized version of optAlignments
-optAlignments' :: String -> String -> [AlignmentType]
-optAlignments' _ _ = []
+optAlignments :: String -> String -> [AlignmentType]
+optAlignments _ _ = []
 
 -- Uses a optAlignments function to create a neat output of optimal alignments
 getOutputOptAlignments :: (String -> String -> [AlignmentType]) -> String -> String -> IO ()
 getOutputOptAlignments optAl s1 s2 = do
-        mapM_ printPair $ optAl s1 s2 -- mapM_ executes the IO action printPair for every part of oa, skipping the last empty IO action
-        where printPair (x, y) = do
-              print $ intersperse ' ' x
-              print $ intersperse ' ' y
-
-outputOptAlignments = getOutputOptAlignments optAlignments
+  mapM_ printPair $ optAl s1 s2 -- mapM_ executes the IO action printPair for every part of oa, skipping the last empty IO action
+  where
+    printPair (x, y) = do
+      print $ intersperse ' ' x
+      print $ intersperse ' ' y
 
 outputOptAlignments' = getOutputOptAlignments optAlignments'
+
+outputOptAlignments = getOutputOptAlignments optAlignments
