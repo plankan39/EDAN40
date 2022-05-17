15c237452b4b328f5e1e0bbebbf6e9b498878f3e
Lukas Elmlund <lukas.elmlund@gmail.com>
2022-05-17 14:42:23 +0200

    test program

--- assignment3/Program.hs
+++ assignment3/Program.hs
@@ -1,14 +1,14 @@
-module Program (T, parse, fromString, toString, exec) where
+module Program (K, parse, fromString, toString, exec) where
 
 import qualified Dictionary
 import Parser hiding (T)
 import qualified Statement
 import Prelude hiding (fail, return)
 
-newtype T = Program [Statement.T]
+newtype K = Program [Statement.T]
 
-instance Parse T where
+instance Parse K where
   parse = iter Statement.parse >-> Program
   toString (Program p) = concatMap Statement.toString p
 
-exec = Statement.exec
+exec (Program k) = Statement.exec k
--- assignment3/TestProgram.hs
+++ assignment3/TestProgram.hs
@@ -1,56 +1,61 @@
 {- Test for Program -}
 module TestProgram where
 
+import qualified Dictionary
 import Program
 
-p0, p1, p2, p3, p4 :: Program.T
+p0, p1, p2, p3, p4 :: Program.K
+p0 =
+  fromString
+    "\
+    \read k;\
+    \read n;\
+    \m := 1;\
+    \while n-m do\
+    \  begin\
+    \    if m - m/k*k then\
+    \      skip;\
+    \    else\
+    \      write m;\
+    \    m := m + 1;\
+    \  end"
+p1 =
+  fromString
+    "\
+    \read n;\
+    \read b;\
+    \m := 1;\
+    \s := 0;\
+    \p := 1;\
+    \while n do\
+    \  begin\
+    \    q := n/b;\
+    \    r := n - q*b;\
+    \    write r;\
+    \    s := p*r+s;\
+    \    p := p*10;\
+    \    n :=q;\
+    \  end\
+    \write s;"
 
-p0 = fromString  ("\
-\read k;\
-\read n;\
-\m := 1;\
-\while n-m do\
-\  begin\
-\    if m - m/k*k then\
-\      skip;\
-\    else\
-\      write m;\
-\    m := m + 1;\
-\  end")
-
-p1 = fromString  ("\
-\read n;\
-\read b;\
-\m := 1;\
-\s := 0;\
-\p := 1;\
-\while n do\
-\  begin\
-\    q := n/b;\
-\    r := n - q*b;\
-\    write r;\
-\    s := p*r+s;\                    
-\    p := p*10;\
-\    n :=q;\
-\  end\
-\write s;")
-
-s1 = "\
-\read n;\
-\read b;\
-\m := 1;\
-\s := 0;\
-\p := 1;\
-\while n do\
-\  begin\
-\    q := n/b;\
-\    r := n - q*b;\
-\    write r;\
-\    s := p*r+s;\                    
-\    p := p*10;\
-\    n :=q;\
-\  end\
-\write s;"
+s1 =
+  "\
+  \read n;\
+  \--hejhej\
+  \read b;\
+  \m := 1;\
+  \s := 0;\
+  \p := 1;\
+  \while n do\
+  \  begin\
+  \    q := n/b;\
+  \    r := n - q*b;\
+  \    write r;\
+  \    s := p*r+s;\
+  \    p := p*10;\
+  \    n :=q;\
+  \  end\
+  \write s;"
 
 sp = putStr (toString p0)
 
@@ -60,25 +65,26 @@ p2 = fromString (toString p0)
 
 p3 = fromString (toString p1)
 
-rp0 = Program.exec p0 [3,16]
+rp0 = Program.exec p0 Dictionary.empty [3, 16]
 
-rp1 = Program.exec p1 [1024, 2]
+rp1 = Program.exec p1 Dictionary.empty [1024, 2]
 
-s4 = "\
-\read a;\
-\read b;\
-\-- a comment\n\
-\s := 3;\
-\while a do\
-\  begin\
-\    c := a^s;\
-\    d := 2^a;\
-\    write c;\
-\    write d;\                    
-\    a := a-1;\
-\  end\
-\write a;"
+s4 =
+  "\
+  \read a;\
+  \read b;\
+  \-- a comment\n\
+  \s := 3;\
+  \while a do\
+  \  begin\
+  \    c := a^s;\
+  \    d := 2^a;\
+  \    write c;\
+  \    write d;\
+  \    a := a-1;\
+  \  end\
+  \write a;"
 
 p4 = fromString s4
 
-rp4 = Program.exec p4 [4,4]
+rp4 = Program.exec p4 Dictionary.empty [4, 4]
